267f9b34e19c7b538ca1c3cdde8691209ca29692
classywldd-byte <classy.wldd@gmail.com>
2026-05-25 15:07:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/App/', // Cambia esto a tu nombre de repositorio
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
